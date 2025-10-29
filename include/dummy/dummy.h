fd360f46e629953cfc9c3e4996bc0fc5a8412a4f
lucas <lucas@3qd.io>
2025-10-29 13:28:13 +0000

    setUp2

---
+++ include/dummy/dummy.h
@@ -0,0 +1,8 @@
+#ifndef DUMMY_H
+#define DUMMY_H
+
+#include <stdint.h>
+
+uint8_t dummy_random(void);
+
+#endif /* DUMMY_H */
---
+++ src/dummy.c
@@ -0,0 +1,7 @@
+#include "dummy/dummy.h"
+#include <stdint.h>
+
+uint8_t dummy_random(void)
+{
+    return 4U; // determined by fair dice roll.
+}
